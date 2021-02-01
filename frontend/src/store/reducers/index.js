5409fe49ccc4cfe945cdd204976ee5b3c6d14c46
Mike <mgalvin@tribevr.io>
2021-02-01 12:12:21 -0600

    made reducer and an action folders/files. Wrapped app in router and store provider

---
+++ frontend/src/store/reducers/index.js
@@ -0,0 +1,20 @@
+
+
+const initialState = {
+	isLoading: false,
+	isLoggedIn: true,
+	itemData: [],
+	userData: {},
+	newItemData: {},
+	error: ''
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+
+    }
+        
+}
+
+
+export default reducer;
