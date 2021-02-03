ecec5c48695b1325e26f52949bea03af4a590270
Mike <mgalvin@tribevr.io>
2021-02-02 20:32:20 -0600

    small changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
 import NotFound404 from './pages/notfound/NotFound404'
 import HomePageContainer from './pages/homepage/HomePageContainer'
+import CreateItem from './pages/profilepage/CreateItem'
+
+import styled from "styled-components";
 
 function App() {
 
@@ -20,19 +23,23 @@ function App() {
   return (
     <>
       <div className="App">
+        <AppNav>
+          <Link to='/' style={{ textDecoration: 'none'}} > Home</Link>
+          <Link to='/profile' style={{ textDecoration: 'none'}} > Profile</Link>
+        </AppNav>
+
+
+
         <Switch>
           {/* {views.map(route => {
             return (<AppRoute key={route.path} path={route.path} component={route.component} isPrivate={route.isPrivate} />);
           })} */}
-
-          <Route exact path="/">
-            <HomePageContainer />
-          </Route>
-
+          {/* <Link to='/profile'>Profile</Link> */}
+          <Route exact path="/" component={HomePageContainer}/>
+          <Route path ='/profile' component={CreateItem}/>
           {/* <Route path="*">
             <NotFound404 />
           </Route> */}
-
         </Switch>
       </div>
     </>
@@ -40,3 +47,9 @@ function App() {
 }
 
 export default App;
+
+const AppNav = styled.nav`
+    padding: 2% 4%;
+    font-size: 2rem;
+    text-align: right;
+`
--- frontend/src/pages/homepage/HomePageContainer.js
+++ frontend/src/pages/homepage/HomePageContainer.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import styled from "styled-components";
 // import { Link } from "react-router-dom"
 import Login from '../profilepage/Login'
 import CreateUser from "./CreateUser";
@@ -11,7 +10,7 @@ const HomePageContainer = props => {
     return (
         <> 
             <h2> Home page</h2>
-            <img alt="" src="https://png.pngtree.com/thumb_back/fh260/background/20200425/pngtree-technology-sense-creative-hand-painted-integrated-circuit-background-image_334505.jpg" />
+            <img alt="circuit board" src="https://png.pngtree.com/thumb_back/fh260/background/20200425/pngtree-technology-sense-creative-hand-painted-integrated-circuit-background-image_334505.jpg" />
             <Homebody>
                 <TechContainer>
                     <HomePageTechCard />
