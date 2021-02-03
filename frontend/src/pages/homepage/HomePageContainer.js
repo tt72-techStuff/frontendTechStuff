e1c49b89990a00cd8414d1ea49c1e56f55aae3ea
Ironipad64 <Ironipad64@gmail.com>
2021-02-03 09:31:09 -0800

    worked more on cards

--- frontend/src/pages/homepage/HomePageContainer.js
+++ frontend/src/pages/homepage/HomePageContainer.js
@@ -6,8 +6,36 @@ import styled from "styled-components";
 import HomePageTechCard from './HomePageTechCard'
 
 const HomePageContainer = props => {
+    // const { values, submit, change, disabled, errors } = props;
+    // const onSubmit = (evt) => {
+    //     evt.preventDefault();
+    //     submit();
+    // };
+    // const onChange = (evt) => {
+    //     const { name, value, type, checked } = evt.target;
+    //     change(name);
+    // }
+
+
     return (
         <>
+            <form>
+                <label>
+                    Category
+          <select name="category">
+                        <option value="">- category -</option>
+                        <option value="medium">camera</option>
+                        <option value="large">video</option>
+                        <option value="audio">audio</option>
+                        <option value="gaming">gaming</option>
+                    </select>
+                </label>
+                <label>
+                    Description:
+          <input type='text' name="description" />
+                </label>
+                <button id="subBtn">submit</button>
+            </form>
             <h2> Home page</h2>
             <img alt="" src="https://png.pngtree.com/thumb_back/fh260/background/20200425/pngtree-technology-sense-creative-hand-painted-integrated-circuit-background-image_334505.jpg" />
             <Homebody>
